7365a5ac4640ee929e638b2d9026b9ed61566fb3
John Heminghous <jheminghous@gmail.com>
2020-09-02 22:33:47 -0400

    Changed reference navigation from tabs to expandable list items

--- app/src/main/java/io/jheminghous/rapidbible/ReferenceAdapter.java
+++ app/src/main/java/io/jheminghous/rapidbible/ReferenceAdapter.java
@@ -1,5 +1,6 @@
 package io.jheminghous.rapidbible;
 
+import android.content.Context;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -7,22 +8,36 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class ReferenceAdapter extends RecyclerView.Adapter<ReferenceAdapter.ViewHolder>
 {
+    private BibleProvider _provider;
+
+    private RecyclerView _recyclerView;
+    private GridLayoutManager _gridLayoutManager;
+
+    private ReferenceAdapter _adapter;
+
     private BibleItem _root;
 
-    private Listener _listener;
+    private ExpandableViewHolder _expanded;
 
-    interface Listener
+    ReferenceAdapter(Context context, @NonNull BibleProvider provider)
     {
-        void onItemSelected(BibleItem item, boolean hardSelect);
-    }
+        _provider = provider;
 
-    ReferenceAdapter(Listener listener)
-    {
-        _listener = listener;
+        if (context != null)
+        {
+            _recyclerView = new RecyclerView(context);
+
+            _gridLayoutManager = new GridLayoutManager(context, 4);
+            _recyclerView.setLayoutManager(_gridLayoutManager);
+
+            _adapter = new ReferenceAdapter(null, provider);
+            _recyclerView.setAdapter(_adapter);
+        }
     }
 
     BibleItem getRoot()
@@ -36,24 +51,36 @@ public class ReferenceAdapter extends RecyclerView.Adapter<ReferenceAdapter.View
         notifyDataSetChanged();
     }
 
+    void select(BibleItem item)
+    {
+        if (_adapter != null)
+        {
+            _adapter.setRoot(item);
+        }
+    }
+
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
     {
         LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-        return new ViewHolder(inflater.inflate(R.layout.selectable_list_item, parent, false));
+
+        if (_recyclerView == null)
+        {
+            return new SimpleViewHolder(inflater.inflate(R.layout.selectable_list_item,
+                                                         parent,
+                                                         false));
+        }
+
+        return new ExpandableViewHolder(inflater.inflate(R.layout.expandable_list_item,
+                                                         parent,
+                                                         false));
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position)
     {
         holder.apply(_root.getChildren().get(position));
-    }
-
-    @Override
-    public int getItemViewType(int position)
-    {
-        return _root.getChildren().get(position).getType().ordinal();
     }
 
     @Override
@@ -64,39 +91,13 @@ public class ReferenceAdapter extends RecyclerView.Adapter<ReferenceAdapter.View
 
     class ViewHolder extends RecyclerView.ViewHolder
     {
-        private TextView _textView;
+        protected TextView _textView;
 
-        private BibleItem _item;
+        protected BibleItem _item;
 
-        ViewHolder(@NonNull View itemView)
+        public ViewHolder(@NonNull View itemView)
         {
             super(itemView);
-
-            _textView = (TextView) itemView;
-            _textView.setOnClickListener(new View.OnClickListener()
-            {
-                @Override
-                public void onClick(View v)
-                {
-                    if (_listener != null)
-                    {
-                        _listener.onItemSelected(_item, false);
-                    }
-                }
-            });
-            _textView.setOnLongClickListener(new View.OnLongClickListener()
-            {
-                @Override
-                public boolean onLongClick(View v)
-                {
-                    if (_listener != null)
-                    {
-                        _listener.onItemSelected(_item, true);
-                    }
-
-                    return true;
-                }
-            });
         }
 
         void apply(BibleItem item)
@@ -115,6 +116,111 @@ public class ReferenceAdapter extends RecyclerView.Adapter<ReferenceAdapter.View
                     _textView.setGravity(Gravity.CENTER);
                     break;
             }
+        }
+    }
+
+    class SimpleViewHolder extends ViewHolder
+    {
+        SimpleViewHolder(@NonNull View itemView)
+        {
+            super(itemView);
+
+            _textView = (TextView) itemView;
+
+            _textView.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v)
+                {
+                    _provider.setCurrentItem(_item);
+                }
+            });
+        }
+    }
+
+    class ExpandableViewHolder extends ViewHolder
+    {
+        protected ViewGroup _expandableLayout;
+
+        ExpandableViewHolder(@NonNull View itemView)
+        {
+            super(itemView);
+
+            _textView = itemView.findViewById(R.id.text);
+            _expandableLayout = itemView.findViewById(R.id.expandable_layout);
+
+            _textView.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v)
+                {
+                    if (_expandableLayout.getVisibility() != View.VISIBLE)
+                    {
+                        if (_item.getChildren().size() == 1)
+                        {
+                            _provider.setCurrentItem(_item);
+                            return;
+                        }
+
+                        _adapter.setRoot(_item);
+                        expand();
+                    }
+                    else
+                    {
+                        collapse();
+                        _adapter.setRoot(null);
+                    }
+                }
+            });
+            _textView.setOnLongClickListener(new View.OnLongClickListener()
+            {
+                @Override
+                public boolean onLongClick(View v)
+                {
+                    _provider.setCurrentItem(_item);
+                    return true;
+                }
+            });
+
+            _expandableLayout.setVisibility(View.GONE);
+        }
+
+        @Override
+        void apply(BibleItem item)
+        {
+            super.apply(item);
+
+            if (_item == _adapter.getRoot() && _item.getChildren().size() > 1)
+            {
+                expand();
+            }
+            else
+            {
+                collapse();
+            }
+        }
+
+        void expand()
+        {
+            if (_expanded == this) return;
+
+            if (_expanded != null)
+            {
+                _expanded.collapse();
+            }
+
+            _expanded = this;
+            _expandableLayout.addView(_recyclerView);
+            _expandableLayout.setVisibility(View.VISIBLE);
+        }
+
+        void collapse()
+        {
+            if (_expanded != this) return;
+
+            _expandableLayout.setVisibility(View.GONE);
+            _expandableLayout.removeView(_recyclerView);
+            _expanded = null;
         }
     }
 }
--- app/src/main/java/io/jheminghous/rapidbible/ReferenceFragment.java
+++ app/src/main/java/io/jheminghous/rapidbible/ReferenceFragment.java
@@ -1,7 +1,6 @@
 package io.jheminghous.rapidbible;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,27 +8,16 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.google.android.material.tabs.TabLayout;
-
 public class ReferenceFragment extends Fragment
 {
-    private static final String TAG = ReferenceFragment.class.getSimpleName();
-
     private BibleProvider _provider;
-
-    private TabLayout _tabLayout;
 
     private RecyclerView _recyclerView;
     private LinearLayoutManager _linearLayoutManager;
-    private GridLayoutManager _gridLayoutManager;
-
     private ReferenceAdapter _adapter;
-
-    private BibleItem _currentItem;
 
     @Nullable
     @Override
@@ -47,22 +35,15 @@ public class ReferenceFragment extends Fragment
                                                BibleProvider.class.getSimpleName());
         }
 
-        View view = inflater.inflate(R.layout.reference, container, false);
+        _recyclerView = (RecyclerView) inflater.inflate(R.layout.list, container, false);
 
-        _tabLayout = view.findViewById(R.id.tab_layout);
-        _tabLayout.addOnTabSelectedListener(_tabListener);
-
-        _recyclerView = view.findViewById(R.id.list);
         _linearLayoutManager = new LinearLayoutManager(getContext());
-        _gridLayoutManager = new GridLayoutManager(getContext(), 4);
         _recyclerView.setLayoutManager(_linearLayoutManager);
 
-        _adapter = new ReferenceAdapter(_referenceListener);
+        _adapter = new ReferenceAdapter(getContext(), _provider);
         _recyclerView.setAdapter(_adapter);
 
-        _currentItem = _provider.getCurrentItem();
-
-        return view;
+        return _recyclerView;
     }
 
     @Override
@@ -70,173 +51,41 @@ public class ReferenceFragment extends Fragment
     {
         super.onResume();
 
-        _tabListener.onTabSelected(_tabLayout.getTabAt(0));
+        _provider.setTitle("Reference");
+
+        BibleItem book;
+
+        BibleItem currentItem = _provider.getCurrentItem();
+        switch (currentItem.getType())
+        {
+            default:
+            case BOOK:
+                book = currentItem;
+                break;
+            case CHAPTER:
+                book = currentItem.getParent();
+                break;
+            case VERSE:
+                book = currentItem.getParent().getParent();
+                break;
+        }
+
+        _adapter.setRoot(book.getParent());
+        _adapter.select(book);
+        _linearLayoutManager.scrollToPosition(book.getNumber() - 1);
     }
 
     @Override
     public void onDestroyView()
     {
-        _tabLayout.removeOnTabSelectedListener(_tabListener);
-        _tabLayout = null;
-
         _recyclerView = null;
 
         _adapter = null;
 
         _linearLayoutManager = null;
-        _gridLayoutManager = null;
 
         _provider = null;
 
         super.onDestroyView();
     }
-
-    private TabLayout.OnTabSelectedListener _tabListener = new TabLayout.OnTabSelectedListener()
-    {
-        @Override
-        public void onTabSelected(TabLayout.Tab tab)
-        {
-            int position = tab.getPosition();
-
-            BibleItem root = null;
-            int scrollPosition = 0;
-
-            if (position == 0)
-            {
-                _recyclerView.setLayoutManager(_linearLayoutManager);
-
-                switch (_currentItem.getType())
-                {
-                    case BOOK:
-                        root = _currentItem.getParent();
-                        scrollPosition = _currentItem.getNumber() - 1;
-                        break;
-                    case CHAPTER:
-                        root = _currentItem.getParent().getParent();
-                        scrollPosition = _currentItem.getParent().getNumber() - 1;
-                        break;
-                    case VERSE:
-                        root = _currentItem.getParent().getParent().getParent();
-                        scrollPosition = _currentItem.getParent().getParent().getNumber() - 1;
-                        break;
-                }
-
-                _adapter.setRoot(root);
-                _linearLayoutManager.scrollToPosition(scrollPosition);
-
-                _provider.setTitle("Reference");
-            }
-            else if (position == 1)
-            {
-                _recyclerView.setLayoutManager(_gridLayoutManager);
-
-                switch (_currentItem.getType())
-                {
-                    case BOOK:
-                        root = _currentItem;
-                        scrollPosition = 0;
-                        break;
-                    case CHAPTER:
-                        root = _currentItem.getParent();
-                        scrollPosition = _currentItem.getNumber() - 1;
-                        break;
-                    case VERSE:
-                        root = _currentItem.getParent().getParent();
-                        scrollPosition = _currentItem.getParent().getNumber() - 1;
-                        break;
-                }
-
-                _adapter.setRoot(root);
-                _gridLayoutManager.scrollToPosition(scrollPosition);
-
-                _provider.setTitle("Reference - " + _adapter.getRoot().getText());
-            }
-            else if (position == 2)
-            {
-                _recyclerView.setLayoutManager(_gridLayoutManager);
-
-                switch (_currentItem.getType())
-                {
-                    case BOOK:
-                        root = _currentItem.getChildren().get(0);
-                        scrollPosition = 0;
-                        break;
-                    case CHAPTER:
-                        root = _currentItem;
-                        scrollPosition = 0;
-                        break;
-                    case VERSE:
-                        root = _currentItem.getParent();
-                        scrollPosition = _currentItem.getNumber() - 1;
-                        break;
-                }
-
-                if (root == null)
-                {
-                    Log.wtf(TAG, "The root item is null");
-                    return;
-                }
-
-                _adapter.setRoot(root);
-                _gridLayoutManager.scrollToPosition(scrollPosition);
-
-                _provider.setTitle("Reference - " + root.getParent().getText() + " " +
-                                   root.getNumber());
-            }
-        }
-
-        @Override
-        public void onTabUnselected(TabLayout.Tab tab)
-        {
-            // Do nothing
-        }
-
-        @Override
-        public void onTabReselected(TabLayout.Tab tab)
-        {
-            // Do nothing
-        }
-    };
-
-    private ReferenceAdapter.Listener _referenceListener = new ReferenceAdapter.Listener()
-    {
-        @Override
-        public void onItemSelected(BibleItem item, boolean hardSelect)
-        {
-            _currentItem = item;
-
-            switch (_currentItem.getType())
-            {
-                case BOOK:
-                    if (!hardSelect)
-                    {
-                        TabLayout.Tab chapterTab = _tabLayout.getTabAt(1);
-                        if (chapterTab == null)
-                        {
-                            Log.wtf(TAG, "The chapter tab is null");
-                            return;
-                        }
-                        chapterTab.select();
-                        break;
-                    }
-                    // Intentional fallthrough
-                case CHAPTER:
-                    if (!hardSelect)
-                    {
-                        TabLayout.Tab verseTab = _tabLayout.getTabAt(2);
-                        if (verseTab == null)
-                        {
-                            Log.wtf(TAG, "The verse tab is null");
-                            return;
-                        }
-                        verseTab.select();
-                        break;
-                    }
-                // Intentional fallthrough
-                case VERSE:
-                    _provider.setCurrentItem(_currentItem);
-                    break;
-            }
-        }
-    };
 }
